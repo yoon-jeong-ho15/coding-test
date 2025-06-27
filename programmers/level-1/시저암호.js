7c031b96df60450b141199612b12b30e064c3940
Yoon Jeong Ho <yoonjh1015@gmail.com>
2025-06-27 22:40:12 +0900

    시저암호 하는중

---
+++ programmers/level-1/시저암호.js
@@ -0,0 +1,39 @@
+function solution(s, n) {
+    const map ={
+        "a":1, "b":2, "c":3, "d":4, "e":5,
+        "f":6, "g":7, "h":8, "i":9, "j":10,
+        "k":11, "l":12, "m":13, "n":14, "o":15,
+        "p":16, "q":17, "r":18, "s":19, "t":20, 
+        "u":21, "v":22, "w":23, "x":24, "y":25,
+        "z":26
+    }
+    
+    var answer = '';
+    for(let i=0; i<s.length;i++){
+        let ch = s[i];
+        let isUpper = false;
+        if (ch==" ") answer += " ";
+        if (ch == ch.toUpperCase()) {
+            isUpper = true;
+            ch = ch.toLowerCase();
+        }
+        console.log(map.entries());
+        let a = (map[ch]+n)%26;
+        let newChar = "";
+        // for(let [key,value] of map.entries()){
+        //     if (value == a){
+        //         newChar = key;
+        //         break;
+        //     }
+        // }
+        console.log(newChar);
+        if(isUpper){
+            answer += newChar.toUpperCase();
+        }else{
+            answer += newChar;
+        }
+    }
+    return answer;
+}
+
+console.log(solution("AB",1));
